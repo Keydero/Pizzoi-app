86bf139e40d36001ed290ff2b3a44f4ae2029008
Oussama Keddar <oussama.keddar@epitech.eu>
2019-04-03 04:32:14 +0200

    Refactor the listView, and use it custom constructor to render only the data needed to be on the screen

--- lib/products.dart
+++ lib/products.dart
@@ -3,21 +3,22 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products; // final == Not changeable
   Products(this.products); // assign the injected products to products
-  @override
+
+  _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/images/pizza.png'),
+          Text(products[index])
+        ],
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
-    return ListView(
-      children: products
-          .map(
-            (product) => Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/images/pizza.png'),
-                      Text(product)
-                    ],
-                  ),
-                ),
-          )
-          .toList(),
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
     );
   }
 }
