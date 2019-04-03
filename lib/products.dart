8f20b6194b08dab0a82bb3232ab217c496267ad9
Oussama Keddar <oussama.keddar@epitech.eu>
2019-04-03 05:23:52 +0200

    Did some refactor to render a conditional render by method

--- lib/products.dart
+++ lib/products.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
   final List<String> products; // final == Not changeable
-  Products(this.products); // assign the injected products to products
+  Products([this.products = const []]) {
+    print('HELLO PRODUCT WIDGET');
+  } // assign the injected products to products
 
   Widget _buildProductItem(BuildContext context, int index) {
     return Card(
@@ -15,12 +17,23 @@ class Products extends StatelessWidget {
     );
   }
 
+  Widget _buildProductsList() {
+    Widget productCard;
+    if (products.length > 0) {
+      productCard = ListView.builder(
+        itemBuilder: _buildProductItem,
+        itemCount: products.length,
+      );
+    } else {
+      return productCard = Center(
+        child: Text('Oh man, 0 products over here'),
+      );
+    }
+    return productCard;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return products.length > 0
-        ? ListView.builder(
-            itemBuilder: _buildProductItem,
-            itemCount: products.length,
-          )
-        : Center(child: Text('No items available'));
+    return _buildProductsList();
   }
 }
